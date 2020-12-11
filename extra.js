2da65cb16a300c1a4c766586c98e71706cb924b0
blackpintz <rosewanjohi24@gmail.com>
2020-12-11 15:50:40 +0300

    refactored the code using hooks.

---
+++ extra.js
@@ -0,0 +1,51 @@
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       total: null,
+//       next: null,
+//       operation: null,
+//       display: '0',
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick = name => {
+//     const { total, next, operation } = this.state;
+//     const result = Calculator({ total, next, operation }, name);
+//     switch (name) {
+//       case '=':
+//         this.setState(state => ({
+//           display: state.operation === null || state.next === null ? state.total : result.total,
+//           operation: null,
+//           next: null,
+//           total: null,
+//         }));
+//         break;
+//       case '+':
+//       case '-':
+//       case '*':
+//       case '/':
+//         this.setState(state => ({
+//           ...result, total: state.display === '0' || state.display === 'syntax error' ? total : state.display, operation: state.display === 'syntax error' ? operation : name,
+//         }));
+//         break;
+//       case 'AC':
+//         this.setState({ ...result, display: '0' });
+//         break;
+//       default:
+//         this.setState({ ...result, display: '0' });
+//         break;
+//     }
+//   };
+
+//   render() {
+//     const { next, total, display } = this.state;
+//     return (
+//       <>
+//         <Display result={next === null ? total : next} display={display} />
+//         <ButtonPanel clickBtn={this.handleClick} />
+//       </>
+//     );
+//   }
+// }
--- src/components/App.js
+++ src/components/App.js
@@ -1,59 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculator from '../logic/calculate';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-      display: '0',
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+function App() {
+  const [total, setTotal] = useState(null);
+  const [next, setNext] = useState(null);
+  const [operation, setOperation] = useState(null);
+  const [display, setDisplay] = useState('0');
 
-  handleClick = name => {
-    const { total, next, operation } = this.state;
+  const handleClick = name => {
     const result = Calculator({ total, next, operation }, name);
     switch (name) {
       case '=':
-        this.setState(state => ({
-          display: state.operation === null || state.next === null ? state.total : result.total,
-          operation: null,
-          next: null,
-          total: null,
-        }));
+        setDisplay(operation === null || next === null ? total : result.total);
+        setOperation(null);
+        setNext(null);
+        setTotal(null);
         break;
       case '+':
       case '-':
       case '*':
       case '/':
-        this.setState(state => ({
-          ...result, total: state.display === '0' || state.display === 'syntax error' ? total : state.display, operation: state.display === 'syntax error' ? operation : name,
-        }));
-        break;
-      case 'AC':
-        this.setState({ ...result, display: '0' });
+        setTotal(display === '0' || display === 'syntax error' ? total : display);
+        setOperation(display === 'syntax error' ? operation : name);
         break;
       default:
-        this.setState({ ...result, display: '0' });
+        setDisplay('0');
+        setTotal(result.total);
+        setNext(result.next);
+        setOperation(result.operation);
         break;
     }
   };
 
-  render() {
-    const { next, total, display } = this.state;
-    return (
-      <>
-        <Display result={next === null ? total : next} display={display} />
-        <ButtonPanel clickBtn={this.handleClick} />
-      </>
-    );
-  }
+  return (
+    <>
+      <Display result={next === null ? total : next} display={display} />
+      <ButtonPanel clickBtn={handleClick} />
+    </>
+  );
 }
 
 export default App;
