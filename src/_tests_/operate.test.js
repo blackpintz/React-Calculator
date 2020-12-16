50fcf7061a7b09e134360a3aecad422075cf520d
blackpintz <rosewanjohi24@gmail.com>
2020-12-16 15:36:21 +0300

    fix linter errors.

--- src/_tests_/operate.test.js
+++ src/_tests_/operate.test.js
@@ -4,35 +4,35 @@ let firstNum;
 let secondNum;
 let thirdNum;
 
-describe("calculator operations", () => {
-    beforeEach(() => {
-         firstNum = '2'
-         secondNum = '4'
-         thirdNum = '0'
-    }); 
+describe('calculator operations', () => {
+  beforeEach(() => {
+    firstNum = '2';
+    secondNum = '4';
+    thirdNum = '0';
+  });
 
-    test("add operation", () => {
-        const result = operate(firstNum, secondNum, "+");
-        expect(result).toEqual("6");
-    });
+  test('add operation', () => {
+    const result = operate(firstNum, secondNum, '+');
+    expect(result).toEqual('6');
+  });
 
-    test("minus operation", () => {
-        const result = operate(firstNum, secondNum, "-")
-        expect(result).toEqual("-2")
-    });
+  test('minus operation', () => {
+    const result = operate(firstNum, secondNum, '-');
+    expect(result).toEqual('-2');
+  });
 
-    test("multiply operation", () => {
-        const result = operate(firstNum, secondNum, "*");
-        expect(result).toEqual("8");
-    })
+  test('multiply operation', () => {
+    const result = operate(firstNum, secondNum, '*');
+    expect(result).toEqual('8');
+  });
 
-    test("divide operation", () => {
-        const result = operate(firstNum, secondNum, "/");
-        expect(result).toEqual("0.5");
-    })
+  test('divide operation', () => {
+    const result = operate(firstNum, secondNum, '/');
+    expect(result).toEqual('0.5');
+  });
 
-    test("divide operation for when second value is zero", () => {
-        const result = operate(firstNum, thirdNum, "/");
-        expect(result).toEqual("syntax error");
-    })
-})
+  test('divide operation for when second value is zero', () => {
+    const result = operate(firstNum, thirdNum, '/');
+    expect(result).toEqual('syntax error');
+  });
+});
--- src/components/App.js
+++ src/components/App.js
@@ -51,7 +51,6 @@ class App extends Component {
 
   render() {
     const { next, total, display } = this.state;
-    console.log(Calculator({ total: '3', next: '4', operation: '*' }, '='));
     return (
       <>
         <Route exact path="/">
