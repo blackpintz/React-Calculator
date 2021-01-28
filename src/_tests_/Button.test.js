d8b12beab3af8672dd8c2244c074698913483f21
Rose Wanjohi <38922385+blackpintz@users.noreply.github.com>
2021-01-28 06:01:44 +0300

    Revert "Testing Math-magicians website"

--- src/_tests_/Button.test.js
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
-import Button from '../components/Button';
-
-test('renders with a className called button', () => {
-  const { container } = render(<BrowserRouter><Button /></BrowserRouter>);
-  expect(container.firstChild).toHaveClass('button');
-});
